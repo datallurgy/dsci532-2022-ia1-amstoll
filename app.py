56f081b68f8698762ed7e6273793c29cd86d7921
datallurgy <allyson@datallurgy.com>
2022-02-19 12:05:00 -0800

    added app.server to app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ iris = data.iris()
 
 ## Setup app and layout/frontend
 app = Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
+server = app.server
 
 app.layout = html.Div([
         html.Iframe(
